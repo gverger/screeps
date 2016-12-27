19e326e71102e4aa2191a764c6456b6a699dbdde
Guillaume Verger <verger.guillaume@gmail.com>
2016-12-27 08:55:32 -0800

    log time spend by creeps

--- main.js
+++ main.js
@@ -4,29 +4,40 @@ var roleBuilder = require('role.builder');
 var roleRepairer = require('role.repairer');
 var roleCarryier = require('role.carryier');
 
-module.exports.loop = function () {
-  require("action.memory.clean").clean();
-  require("action.spawn").spawn(Game.spawns.Spawn1);
-  require("action.build.roads").buildRoads(Game.spawns.Spawn1.room);
-  for(var name in Game.creeps) {
-    var creep = Game.creeps[name];
-    if (creep.spawning) {
-      continue;
+module.exports = {
+  loop:  function () {
+    require("action.memory.clean").clean();
+    require("action.spawn").spawn(Game.spawns.Spawn1);
+    require("action.build.roads").buildRoads(Game.spawns.Spawn1.room);
+    for(var name in Game.creeps) {
+      var creep = Game.creeps[name];
+      if (creep.spawning) {
+        continue;
+      }
+      if (creep.memory.role == "harvester") {
+        this.runRole(roleHarvester, creep);
+      }
+      else if (creep.memory.role == "upgrader" ) {
+        this.runRole(roleUpgrader, creep);
+      }
+      else if (creep.memory.role == "builder" ) {
+        this.runRole(roleBuilder, creep);
+      }
+      else if (creep.memory.role == "repairer" ) {
+        this.runRole(roleRepairer, creep);
+      }
+      else if (creep.memory.role == "carryier" ) {
+        this.runRole(roleCarryier, creep);
+      }
     }
-    if (creep.memory.role == "harvester") {
-      roleHarvester.run(creep);
-    }
-    else if (creep.memory.role == "upgrader" ) {
-      roleUpgrader.run(creep);
-    }
-    else if (creep.memory.role == "builder" ) {
-      roleBuilder.run(creep);
-    }
-    else if (creep.memory.role == "repairer" ) {
-      roleRepairer.run(creep);
-    }
-    else if (creep.memory.role == "carryier" ) {
-      roleCarryier.run(creep);
+  },
+
+  runRole: function(role, creep) {
+    t1 = Game.cpu.getUsed();
+    role.run(creep);
+    t2 = Game.cpu.getUsed();
+    if (t2 - t1 > 3) {
+      console.log(creep.name + ", " + creep.memory.role[0] + " " + creep.memory.status + " : " + (t2 - t1));
     }
   }
 }
