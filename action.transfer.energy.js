d472da6029cf8b3d2710257b72037d0221fc1968
Guillaume Verger <verger.guillaume@gmail.com>
2016-12-29 11:53:39 -0800

    shut up creep!

--- action.transfer.energy.js
+++ action.transfer.energy.js
@@ -6,7 +6,6 @@ var actionTransferEnergy = {
       structures = _.filter(structures, filter);
     if (structures != "") {
       var s = creep.pos.findClosestByPath(structures);
-      creep.say(s.pos.x + " " + s.pos.y);
       if(creep.transfer(s, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(s);
       }
