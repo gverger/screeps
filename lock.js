33c77e38b8f64284d418a3203c093365419d7a6e
Guillaume Verger <verger.guillaume@gmail.com>
2016-12-29 11:52:36 -0800

    print lock

--- lock.js
+++ lock.js
@@ -1,5 +1,6 @@
 var lock = {
   lock: function(entity, resource, score) {
+    this.log("call lock " + this.structureToString(resource) + " -> " + this.entityToString(entity) + " " + score);
     this.init();
     var locked = this.get(resource.id)
     if (locked == undefined) {
@@ -24,22 +25,29 @@ var lock = {
     Memory.lock[resource.id] = { entity: entity.id, score: score };
     if (entity.memory != undefined)
       entity.memory.lock = resource.id;
+    this.prettyPrint();
   },
 
   lockAllResources: function(spawn) {
+    this.log("lockAll");
     var resources = this.resources(spawn.room);
     for(let r of resources) {
       this.lock(spawn, r, 0);
     }
+    this.prettyPrint();
   },
 
   releaseAllResources: function(spawn) {
+    this.log("releaseAll");
     var resources = this.resources(spawn.room);
     for(let r of resources) {
       var locked = this.get(r.id);
-      if (locked != undefined && locked.entity == spawn.id)
+      if (locked != undefined && locked.entity == spawn.id) {
+        this.log("releasing");
         this.remove(r.id);
+      }
     }
+    this.prettyPrint();
   },
 
   resources: function(room) {
@@ -65,6 +73,7 @@ var lock = {
     if (entity != undefined)
       delete entity.memory.lock;
     // console.log(Object.keys(Memory.lock).length);
+    this.prettyPrint();
   },
 
   releaseCreep: function(entity) {
@@ -75,6 +84,7 @@ var lock = {
     }
     delete entity.memory.lock
     // console.log(Object.keys(Memory.lock).length);
+    this.prettyPrint();
   },
 
   clean: function() {
@@ -88,7 +98,7 @@ var lock = {
     }
     for (var i = 0, len = to_remove.length; i < len; i++) {
       var key = to_remove[i]
-      console.log("Deleting " + key);
+      this.log("Deleting " + key);
       delete Memory.lock[key];
     }
   },
@@ -105,6 +115,33 @@ var lock = {
     if (Memory.lock == undefined) {
       Memory.lock = {};
     }
+  },
+
+  prettyPrint: function() {
+    this.log("--- LOCK ---");
+    for (let key in Memory.lock) {
+      var locked = Memory.lock[key];
+      var creep = Game.getObjectById(locked.entity);
+      var structure = Game.getObjectById(key);
+      this.log(this.structureToString(structure) + " -> " + this.entityToString(creep) + " " + locked.score);
+    }
+    this.log("------------");
+  },
+
+  entityToString: function(object) {
+    return object.name;
+  },
+
+  structureToString: function(object) {
+    return object.structureType + this.posToString(object);
+  },
+
+  posToString: function(object) {
+    return "[" + object.pos.x + " " + object.pos.y + "]";
+  },
+
+  log: function(message) {
+    // console.log("["+Game.time+"]"+message);
   }
 };
 
