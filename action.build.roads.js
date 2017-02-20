ebaaf0c55c2cbb9fd39c9c27ab5314948946960b
Guillaume Verger <verger.guillaume@gmail.com>
2017-02-20 15:54:34 -0800

    build roads around extensions

--- action.build.roads.js
+++ action.build.roads.js
@@ -8,14 +8,13 @@ var actionBuildRoads = {
     if (Game.time % 10 != 0) {
       return null;
     }
-    //this.usedPoints(room);
     var sources = room.find(FIND_SOURCES);
     for (let source of sources) {
       this.buildTheRoad(room, room.controller.pos, source.pos);
       var spawn = utils.spawnOfRoom(room);
       this.buildTheRoad(room, spawn.pos, source.pos);
-      Memory.roads = true;
     }
+    this.buildRoadsAroundExtensions(room);
   },
 
   buildTheRoad: function(room, pos1, pos2) {
@@ -26,69 +25,27 @@ var actionBuildRoads = {
     };
   },
 
-  /* @function usedPoints
-   * @param {Room}
-   */
-  usedPoints: function(room) {
-    if (room.memory.usedPoints == undefined) {
-      room.memory.usedPoints = (new PathFinder.CostMatrix).serialize();
-    }
-    /**
-     * @type {PathFinder.CostMatrix}
-     **/
-    var points = PathFinder.CostMatrix.deserialize(room.memory.usedPoints);
-    var creeps = Game.creeps;
-    for (var name in creeps) {
-      /**
-       * @type {Creep}
-       **/
-      var creep = Game.creeps[name];
-      if (creep.room != room || creep.fatigue > 0) {
-        continue;
+  buildRoadsAroundExtensions: function(room) {
+    let extensions = room.find(FIND_MY_STRUCTURES,
+        { filter: { structureType: STRUCTURE_EXTENSION } });
+    let positions = _(extensions).map(function(extension) {
+      /** @type {RoomPosition} pos */
+      let pos = extension.pos;
+      return [
+        new RoomPosition(pos.x, pos.y + 1, pos.roomName),
+        new RoomPosition(pos.x, pos.y - 1, pos.roomName),
+        new RoomPosition(pos.x + 1, pos.y, pos.roomName),
+        new RoomPosition(pos.x - 1, pos.y, pos.roomName)
+      ];
+    }).flatten().value();
+    _.each(positions, function(position) {
+      let structures = room.lookForAt('structure', position);
+      if (_.all(structures, function(s) {
+        return !OBSTACLE_OBJECT_TYPES.includes(s.structureType);
+      })) {
+        room.createConstructionSite(position.x, position.y, STRUCTURE_ROAD);
       }
-      points.set(creep.pos.x, creep.pos.y, points.get(creep.pos.x, creep.pos.y) + 1);
-    }
-
-    this.bestRoutePoint(points, room);
-    room.memory.usedPoints = points.serialize();
-  },
-
-  /**
-   * @function bestRoutePoint
-   * @param {PathFinder.CostMatrix} matrix
-   * @param {Room} room
-   **/
-  bestRoutePoint: function(matrix, room) {
-    if (Game.time % 10 != 0) {
-      return;
-    }
-    var max = 0;
-    var maxX = 0;
-    var maxY = 0;
-    var isRoad = new PathFinder.CostMatrix;
-    var roads = room.find(FIND_STRUCTURES);
-    for (let road of roads) {
-      isRoad.set(road.pos.x, road.pos.y, 1);
-    }
-    for (var x = 0; x < 50; x++) {
-      for (var y = 0; y < 50; y++) {
-        if (isRoad.get(x, y)) {
-          continue;
-        }
-        var value = matrix.get(x, y);
-        if (max < value) {
-          max = value;
-          maxX = x;
-          maxY = y;
-        }
-        // if (value > 0) {
-        //   matrix.set(x, y, value - 1);
-        // }
-      }
-    }
-    if (max > 10) {
-      room.createConstructionSite(maxX, maxY, STRUCTURE_ROAD);
-    }
+    });
   }
 };
 
